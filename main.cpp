e6672fa88bd2bdc558bbb70ac9e18ba843b3d7c3
Nguyễn Phúc Thuần <phucthuan1st@gmail.com>
2022-03-17 00:48:37 +0700

    fix modulo, display

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "BinaryOperation.h"
 
 int main() {
-    Integer first_num(38);
-    Integer second_num(13);
+    Integer first_num(10);
+    Integer second_num(5);
 
     Integer add  = first_num + second_num;
     Integer sub = first_num - second_num;
@@ -10,10 +10,17 @@ int main() {
     Integer divide = first_num / second_num;
     Integer remainder = first_num % second_num;
 
+    first_num.print();
+    second_num.print();
+    printf("A + B = %d + %d\n", first_num.decimal(), second_num.decimal());
     add.print();
+    printf("A - B = %d - %d\n", first_num.decimal(), second_num.decimal());
     sub.print();
+    printf("A * B = %d * %d\n", first_num.decimal(), second_num.decimal());
     multiply.print();
+    printf("A / B = %d / %d\n", first_num.decimal(), second_num.decimal());
     divide.print();
+    printf("A %% B = %d %% %d\n", first_num.decimal(), second_num.decimal());
     remainder.print();
 
     return 0;
